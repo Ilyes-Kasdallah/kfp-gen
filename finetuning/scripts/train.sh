1f70e2a7c577bdc784c5f459ca4b71845af313e3
Ilyes Kasdallah <ilyeskasdallah123@gmail.com>
2025-08-06 18:34:50 +0100

    changes in dataset creation script

--- finetuning/scripts/train.sh
+++ finetuning/scripts/train.sh
@@ -5,7 +5,7 @@ source ~/ENV/bin/activate
 cd /home/ilyes/scratch/kfp-gen/finetuning
 pip install -r requirements.txt
 
-python data/prepare_dataset.py
+#python data/prepare_dataset.py
 
 python unsloth_trainer/train_sft.py \
   --model Qwen/Qwen1.5-8B \
