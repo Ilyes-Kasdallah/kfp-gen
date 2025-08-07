86667bb0673e9b10dfee7c2612bbbe3b84853eb1
Ilyes Kasdallah <ilyeskasdallah123@gmail.com>
2025-08-07 04:50:04 +0100

    changes in

--- finetuning/init.sh
+++ finetuning/init.sh
@@ -38,9 +38,9 @@ if [ "$1" ]; then
   MODEL_ID="$1"
   MODEL_DIR="$SCRATCH/models/$MODEL_ID"
   mkdir -p "$MODEL_DIR"
-  source /scratch/cherif/dataset/token.sh
+  #source /scratch/cherif/dataset/token.sh
   echo "Downloading HF model $MODEL_ID to $MODEL_DIR"
-  huggingface-cli download "$MODEL_ID" --local-dir "$MODEL_DIR" --token "$HF_TOKEN"
+  huggingface-cli download "$MODEL_ID" --local-dir "$MODEL_DIR" 
 fi
 
 #############################
