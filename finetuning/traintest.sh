675ab090a6930d1c6287a68dcd196e0844df89fa
Ilyes Kasdallah <ilyeskasdallah123@gmail.com>
2025-08-07 22:30:34 +0100

    edit model path

--- finetuning/traintest.sh
+++ finetuning/traintest.sh
@@ -24,7 +24,7 @@ export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:128
 # Run fine-tuning script
 python unsloth_trainer/train_sft.py \
   --model /scratch/ilyes/models/Qwen/Qwen2.5-7B-Instruct \
-  --dataset data/prompts_dataset \
+  --dataset /home/ilyes/scratch/kfp-gen/finetuning/data/data/prompts_dataset \
   --run_name "kfp-Qwen2.5-7B-Instruct" \
   --output_dir "run/kfp-Qwen2.5-7B-Instruct" \
   --batch 1 \
